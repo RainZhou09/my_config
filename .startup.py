dd41d8e72f0c6ae8c992396f978b404f6c4f1c51
Rain Zhou <rainzhou1994@gmail.com>
2022-06-14 21:26:34 -0700

    Add files via upload

---
+++ .startup.py
@@ -0,0 +1,24 @@
+# Rain's .startup.py file
+print("(.startup.py)")
+import math
+print("(imported math)")
+pi=math.pi
+
+def cos(x):
+    return math.cos(x)
+def acos(x):
+    return math.acos(x)
+def sin(x):
+    return math.sin(x)
+def asin(x):
+    return math.asin(x)
+def tan(x):
+    return math.tan(x)
+def atan(x):
+    return math.atan(x)
+def rad2deg(x):
+    return math.degrees(x)
+def deg2rad(x):
+    return math.radians(x)
+
+
